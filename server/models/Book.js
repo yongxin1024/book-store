1d374875de0b0289ea73475a07461ac6cffc57a5
zhangyongxin <yzhang7@opentext.com>
2025-02-19 16:33:09 +0800

    add index

--- server/models/Book.js
+++ server/models/Book.js
@@ -7,4 +7,7 @@ const bookSchema = new mongoose.Schema({
     saleCount: {type: Number, default: 0}
 });
 
+// 在 Book 模型添加索引
+bookSchema.index({ saleCount: -1 });
+
 module.exports = mongoose.model('Book', bookSchema);
